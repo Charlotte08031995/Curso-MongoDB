6a524c08a54c0fb5c98799bf42579abbe92f1d16
Charlotte Salazar <125162642+Charlotte08031995@users.noreply.github.com>
2023-09-28 20:19:09 -0500

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -1,1 +1,10 @@
+const galaxies = [3,5,78,54,23,12,45,70];
+// E
+const contarEstrellas = () => {
+   const total = galaxies.reduce(
+      (acumulador, estrella) => acumulador + estrella 
+   )
+   return total
+}
 
+console.log(contarEstrellas(galaxies))
